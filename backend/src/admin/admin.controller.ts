6cfb754f797194045f1154ae0a5bf58843688e50
rxnydash1337 <abcronydash1337@gmail.com>
2025-12-20 05:46:31 +0300

    Update admin.controller.ts

--- backend/src/admin/admin.controller.ts
+++ backend/src/admin/admin.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Delete, Body, Param, UseGuards, UnauthorizedException, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param, UseGuards, UnauthorizedException, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtService } from '@nestjs/jwt';
 import { AdminService } from './admin.service';
