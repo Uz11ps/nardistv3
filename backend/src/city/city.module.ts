742e32113300124851bcb61277020e834c50d975
rxnydash1337 <abcronydash1337@gmail.com>
2025-12-23 16:33:28 +0300

    высоко остаца ноя невернусь наZZZZZZад zad zad zad..zad.az.dzad.zad ass( jope)

--- backend/src/city/city.module.ts
+++ backend/src/city/city.module.ts
@@ -4,12 +4,14 @@ import { CityController } from './city.controller';
 import { CityService } from './city.service';
 import { Building } from './building.entity';
 import { BuildingConfig } from './building-config.entity';
+import { Clan } from '../clans/clan.entity';
+import { User } from '../users/user.entity';
 import { UsersModule } from '../users/users.module';
 import { ClansModule } from '../clans/clans.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Building, BuildingConfig]),
+    TypeOrmModule.forFeature([Building, BuildingConfig, Clan, User]),
     UsersModule,
     forwardRef(() => ClansModule),
   ],
