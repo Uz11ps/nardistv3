b75137cf7c69d9cf2859d82252657795c23e2e9a
rxnydash1337 <abcronydash1337@gmail.com>
2026-01-22 01:24:44 +0300

    Update ratings.module.ts

--- backend/src/ratings/ratings.module.ts
+++ backend/src/ratings/ratings.module.ts
@@ -1,13 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RatingsService } from './ratings.service';
 import { RatingsController } from './ratings.controller';
 import { Rating } from './rating.entity';
 import { SystemSettings } from '../admin/system-settings.entity';
 import { Game } from '../games/game.entity';
+import { SubscriptionModule } from '../subscription/subscription.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Rating, SystemSettings, Game])],
+  imports: [
+    TypeOrmModule.forFeature([Rating, SystemSettings, Game]),
+    forwardRef(() => SubscriptionModule),
+  ],
   controllers: [RatingsController],
   providers: [RatingsService],
   exports: [RatingsService],
